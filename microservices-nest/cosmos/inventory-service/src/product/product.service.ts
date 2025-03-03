81908ce76f19e53ddb622d1415e8a5a4e834c4d6
Kisho11 <Kishanthan.kisho95@gmail.com>
2025-03-04 00:20:16 +0530

    kafka added

--- microservices-nest/cosmos/inventory-service/src/product/product.service.ts
+++ microservices-nest/cosmos/inventory-service/src/product/product.service.ts
@@ -5,13 +5,23 @@ import { Product } from './entity/product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { StockValidationResponseDto } from './dto/stock-validation-response.dto';
 import { BadRequestException } from '@nestjs/common';
+import { Kafka } from 'kafkajs'; 
 
 @Injectable()
 export class ProductsService {
+  private kafka = new Kafka({ brokers: ['localhost:9092'] });
+  private readonly producer = this.kafka.producer();
+  private readonly consumer = this.kafka.consumer({ groupId: 'product-service' });
   constructor(
     @InjectRepository(Product)
     private productsRepository: Repository<Product>,
   ) {}
+
+  async onModuleInit() {
+    await this.producer.connect();
+    await this.consumer.connect();
+    await this.consumeOrderCreated();
+  }
 
   async create(createProductDto: CreateProductDto) {
     const product = this.productsRepository.create(createProductDto);
@@ -60,5 +70,28 @@ export class ProductsService {
     
     product.quantity -= quantity;
     return await this.productsRepository.save(product);
+  }
+
+  async consumeOrderCreated(){
+    await this.consumer.subscribe({ topic: 'kishanthan.order.create' });
+
+    console.log('--subscribe--kishanthan.order.create-----');
+    await this.consumer.run({
+      eachMessage: async ({ message }) => {
+        const { customerId, customerName, items } = JSON.parse(
+          message.value.toString(),
+        );
+
+        for (const item of items) {
+          await this.reduceQuantity(item.productId, item.quantity);
+          
+        }
+        await this.producer.send({
+          topic: 'kishanthan.order.inventory.update',
+          messages: [{value: JSON.stringify({customerId, customerName, items})}],
+        });  
+        console.log('--produce-----Kishanthan.order.inventory.update------');      
+      },
+    });
   }
 }
--- microservices-nest/cosmos/order-service/src/orders/orders.service.ts
+++ microservices-nest/cosmos/order-service/src/orders/orders.service.ts
@@ -2,6 +2,7 @@ import {
     BadRequestException,
     Injectable,
     NotFoundException,
+    OnModuleInit
   } from '@nestjs/common';
   import { InjectRepository } from '@nestjs/typeorm';
   import { Repository } from 'typeorm';
@@ -11,9 +12,14 @@ import {
   import { OrderStatus, UpdateOrderStatus } from './dto/update-order.dto';
   import { HttpService } from '@nestjs/axios';
   import { lastValueFrom } from 'rxjs';
+  import { Kafka } from 'kafkajs';
   
   @Injectable()
   export class OrdersService {
+    private kafka = new Kafka({brokers: ['localhost:9092']});
+    private readonly producer = this.kafka.producer();
+    private readonly consumer = this.kafka.consumer({groupId: 'kishanthan-order-service'});
+
     private readonly inventoryServiceUrl = 'http://localhost:3001/products';
     private readonly customerServiceUrl = 'http://localhost:3002/customers';
     
@@ -24,6 +30,7 @@ import {
       private readonly orderItemRepository: Repository<OrderItem>,
       private readonly httpService: HttpService,
     ) {}
+    
     
     async create(createOrderDto: createOrderDto): Promise<any> {
       const { customerId, items } = createOrderDto;
@@ -41,91 +48,101 @@ import {
           `Customer ID ${customerId} does not exist.`,
         );
       }
-  
-      // Fetch product details and validate stock for all items
-      const itemsWithDetails = await Promise.all(
-        items.map(async (item) => {
-          try {
-            // First get product details to get the price
-            const productResponse$ = this.httpService.get(
-              `${this.inventoryServiceUrl}/${item.productId}`,
-            );
-            const productResponse = await lastValueFrom(productResponse$);
-            const productDetails = productResponse.data;
-            
-            // Then validate stock
-            const stockResponse$ = this.httpService.get(
-              `${this.inventoryServiceUrl}/${item.productId}/validate?quantity=${item.quantity}`,
-            );
-            const stockResponse = await lastValueFrom(stockResponse$);
-            
-            if (!stockResponse.data.available) {
-              throw new BadRequestException(
-                `Product ID ${item.productId} is out of stock.`,
-              );
-            }
-            
-            // Return item with price from product details
-            return {
-              ...item,
-              price: item.price || productDetails.price, // Use provided price or get from product
-            };
-          } catch (error) {
-            throw new BadRequestException(
-              `Error processing Product ID ${item.productId}: ${error.message}`,
-            );
-          }
-        })
-      );
-      
-      // Create order
-      const order = this.orderRepository.create({
-        customerId,
-        status: OrderStatus.PENDING,
-        customerDetails: customerDetails, // Include customer details in the order
+
+
+      this.producer.send({
+        topic: 'kishanthan.order.create',
+        messages: [
+          { value: JSON.stringify({customerId, customerDetails, items}) },
+        ],
       });
+      console.log('--produce-----kishanthan.order.create-------');
+      return {message: 'Order is placed. waitting inventory service to process'};
+  
+      // // Fetch product details and validate stock for all items
+      // const itemsWithDetails = await Promise.all(
+      //   items.map(async (item) => {
+      //     try {
+      //       // First get product details to get the price
+      //       const productResponse$ = this.httpService.get(
+      //         `${this.inventoryServiceUrl}/${item.productId}`,
+      //       );
+      //       const productResponse = await lastValueFrom(productResponse$);
+      //       const productDetails = productResponse.data;
+            
+      //       // Then validate stock
+      //       const stockResponse$ = this.httpService.get(
+      //         `${this.inventoryServiceUrl}/${item.productId}/validate?quantity=${item.quantity}`,
+      //       );
+      //       const stockResponse = await lastValueFrom(stockResponse$);
+            
+      //       if (!stockResponse.data.available) {
+      //         throw new BadRequestException(
+      //           `Product ID ${item.productId} is out of stock.`,
+      //         );
+      //       }
+            
+      //       // Return item with price from product details
+      //       return {
+      //         ...item,
+      //         price: item.price || productDetails.price, // Use provided price or get from product
+      //       };
+      //     } catch (error) {
+      //       throw new BadRequestException(
+      //         `Error processing Product ID ${item.productId}: ${error.message}`,
+      //       );
+      //     }
+      //   })
+      // );
       
-      const savedOrder = await this.orderRepository.save(order);
+      // // Create order
+      // const order = this.orderRepository.create({
+      //   customerId,
+      //   status: OrderStatus.PENDING,
+      //   customerDetails: customerDetails, // Include customer details in the order
+      // });
       
-      // Create order items with prices
-      const orderItems = itemsWithDetails.map((item) =>
-        this.orderItemRepository.create({
-          productId: item.productId,
-          price: item.price,
-          quantity: item.quantity,
-          order: savedOrder,
-        }),
-      );
+      // const savedOrder = await this.orderRepository.save(order);
       
-      const savedOrderItems = await this.orderItemRepository.save(orderItems);
+      // // Create order items with prices
+      // const orderItems = itemsWithDetails.map((item) =>
+      //   this.orderItemRepository.create({
+      //     productId: item.productId,
+      //     price: item.price,
+      //     quantity: item.quantity,
+      //     order: savedOrder,
+      //   }),
+      // );
       
-      // Reduce stock in Inventory Service
-      for (const item of savedOrderItems) {
-        try {
-          console.log(`Reducing stock for Product ID ${item.productId} by ${item.quantity}`);
+      // const savedOrderItems = await this.orderItemRepository.save(orderItems);
+      
+      // // Reduce stock in Inventory Service
+      // for (const item of savedOrderItems) {
+      //   try {
+      //     console.log(`Reducing stock for Product ID ${item.productId} by ${item.quantity}`);
           
-          const response$ = this.httpService.patch(
-            `${this.inventoryServiceUrl}/${item.productId}/quantity`,
-            { quantity: item.quantity },
-          );
+      //     const response$ = this.httpService.patch(
+      //       `${this.inventoryServiceUrl}/${item.productId}/quantity`,
+      //       { quantity: item.quantity },
+      //     );
           
-          await lastValueFrom(response$);
-          console.log(`Stock reduction successful for Product ID ${item.productId}`);
-        } catch (error) {
-          console.error(`Failed to reduce stock for Product ID ${item.productId}:`, error.response?.data || error.message);
-          throw new BadRequestException(
-            `Failed to reduce stock for Product ID ${item.productId}: ${error.message}`,
-          );
-        }
-      }
+      //     await lastValueFrom(response$);
+      //     console.log(`Stock reduction successful for Product ID ${item.productId}`);
+      //   } catch (error) {
+      //     console.error(`Failed to reduce stock for Product ID ${item.productId}:`, error.response?.data || error.message);
+      //     throw new BadRequestException(
+      //       `Failed to reduce stock for Product ID ${item.productId}: ${error.message}`,
+      //     );
+      //   }
+      // }
       
-      // Return the created order with customer details and items
-      return { 
-        ...savedOrder, 
-        customerName: customerDetails.name,
-        customerEmail: customerDetails.email,
-        items: savedOrderItems 
-      };
+      // // Return the created order with customer details and items
+      // return { 
+      //   ...savedOrder, 
+      //   customerName: customerDetails.name,
+      //   customerEmail: customerDetails.email,
+      //   items: savedOrderItems 
+      // };
     }
     
     async fetch(id: number) {
@@ -168,5 +185,40 @@ import {
       
       order.status = updateStatus.status;
       return await this.orderRepository.save(order);
+    }
+
+    async consumeConfirmOrders() {
+      await this.consumer.subscribe({ topic: 'kishanthan.order.inventory.update' });
+      
+      console.log('--subscribe---kishanthan.order.inventory.update----');
+      await this.consumer.run({
+        eachMessage: async ({ message }) => {
+          const { customerId, items } = JSON.parse(
+            message.value.toString(),
+          );
+          // console.log('items', items);
+          const order = this.orderRepository.create({
+            customerId,
+            status: 'CONFIRMED',
+          });
+          const savedOrder = await this.orderRepository.save(order);
+          const orderItems = items.map((item) =>
+            this.orderItemRepository.create({
+              productId: item.productId,
+              price: item.price,
+              quantity: item.quantity,
+              order: savedOrder,
+            }),
+          );
+          console.log("----order.inventory.update-----")
+          await this.orderItemRepository.save(orderItems);
+        },
+      });
+  
+      this.producer.send({
+        topic: 'kishanthan.order.confirmed',
+        messages: [{ value: 'Order service is ready to process orders' }],
+      });
+      console.log('--produce-----kishanthan.order.confirmed-------');
     }
   }
